b470b2ff75b3507c18acc11b9bd48e6438eb1b9c
123210547 ARTHUR CORREIA LEITE <arthur.correia.leite@lcc3-003.lcc.ufcg.edu.br>
2024-12-04 09:39:00 -0300

    Questões no laboraório

---
+++ src/questoes/MultiplicaNElementosSeq.java
@@ -0,0 +1,38 @@
+package questoes;
+
+import java.util.Scanner;
+
+/*
+ * Não consigo encontrar o tst da questão. Testes feitos são apenas manuais.
+ */
+
+public class MultiplicaNElementosSeq {
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        
+        String num = sc.nextLine();
+
+        String[] entrada = sc.nextLine().split(" ");
+        int[] sequenciaNums = new int[entrada.length];
+        for (int i = 0; i < entrada.length; i++) {
+            sequenciaNums[i] = Integer.parseInt(entrada[i]);
+        }
+        
+        sequenciaNums = multiplicador(sequenciaNums, Integer.parseInt(num));
+        for (int i = 0; i < sequenciaNums.length; i++) {
+            if (i == sequenciaNums.length-1) {
+                System.out.println(sequenciaNums[i]);
+            } else {
+                System.out.print(sequenciaNums[i] + " ");
+            }
+        }
+    }
+
+    public static int[] multiplicador(int[] seq, int n) {
+        for (int i = 0; i < seq.length; i++) {
+            seq[i] *= n;
+        }
+        return seq;
+    }
+}
