6737fcf6bf910e98891c82c910683ea704b01154
Arthur Correia <arthurcorreialeite@gmail.com>
2024-12-22 17:54:04 -0300

    adicionada uma questão

---
+++ src/questoes/VerificacaoElementosDuplicados.java
@@ -0,0 +1,25 @@
+package questoes;
+
+import java.util.*;
+
+public class VerificacaoElementosDuplicados {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] entrada = sc.nextLine().split(" ");
+        int[] sequeciaNumerica = new int[entrada.length];
+        for (int i = 0; i < entrada.length; i++) {
+            sequeciaNumerica[i] = Integer.parseInt(entrada[i]);
+        } 
+
+        System.out.println(verificaElementos(sequeciaNumerica));
+    }
+
+    private static boolean verificaElementos(int[] seq) {
+        for (int i = 0; i < seq.length - 1; i++) {
+            for (int j = i + 1; j < seq.length; j++) {
+                if (seq[i] == seq[j]) return true;
+            }
+        }
+        return false;
+    }
+}
